a34370e913cfd4684ae6400495b18c50b9546460
Nhu Bao <giabaosocool@gmail.com>
2024-06-09 22:22:30 +1000

    Add project files.

---
+++ 1.2P/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Message myMessage;
+            Message[] messages = {new Message("Hi Bao, welcome back!"),
+                                new Message("Great to see you again, Huy ^^"),
+                                new Message("Isn't it Duc the showman himself!"),
+                                new Message("Yo, Hung!"),
+                                new Message("Welcome, nice to meet you.")
+                                };
+            string name;
+
+            myMessage = new Message("Hello World!");
+            myMessage.Print();
+
+            while (true)
+            {
+                Console.WriteLine("Enter name: ");
+                name = Console.ReadLine();
+                if (name.ToLower() == "bao")
+                {
+                    messages[0].Print();
+                }
+                else if (name.ToLower() == "huy")
+                {
+                    messages[1].Print();
+                }
+                else if (name.ToLower() == "duc")
+                {
+                    messages[2].Print();
+                }
+                else if (name.ToLower() == "hung")
+                {
+                    messages[3].Print();
+                }
+                else
+                {
+                    messages[4].Print();
+                }
+            }
+
+
+        }
+    }
+}
